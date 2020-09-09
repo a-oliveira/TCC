35f4ababf0926782f5eede73a0a27ab8be695b6d
a-oliveira <dessaschweitzer@gmail.com>
2020-09-08 21:57:53 -0300

    crop faces

---
+++ CapsNet-Keras-tf2.2-adaptado/preprocImg.py
@@ -0,0 +1,124 @@
+import PIL 
+import csv
+import glob, os
+from numpy import *
+from PIL import Image
+from matplotlib import pyplot
+#from resizeimage import resizeimage
+from keras.preprocessing.image import *
+import pandas as pd
+from faces import *
+
+PATH          = os.path.dirname(os.path.abspath(__file__)) + '/UFJF.csv'
+PATH_BKP      = '.../imagens_treino/'
+BASE_UFJF     = os.path.dirname(os.path.abspath(__file__)) + '/ImagensUFJF'
+CONJ_TREINO   = os.path.dirname(os.path.abspath(__file__)) + '/CONJ_TREINO/*.jpg'
+CONJ_TESTE    = os.path.dirname(os.path.abspath(__file__)) + '/CONJ_TESTE/*.jpg'
+
+'''
+LARGURA       = 80
+ALTURA        = 80
+
+'''
+#print(nome(i))
+#pyplot.imshow(pixels) - serve para printar a imagem em forma de gráfico
+
+
+def nome(diretorio):
+
+    nome = diretorio.split("\\") # pega apenas o 'id.jpg'
+    nome = nome[1]
+    nome = nome.split(".jpg")  # segundo split para pegar apenas o id
+    return nome[0]
+
+'''
+def nome(diretorio):
+
+    #nome = diretorio.split('imagensBD2/') # pega apenas o 'id.jpg'
+    nome = diretorio.rsplit('/', 1) # pega apenas o 'id.jpg'
+    print(nome)
+    nome = nome[1]
+    nome = nome.split(".jpg")  # segundo split para pegar apenas o id
+    
+    return nome[0]
+'''
+
+def criarDiretorio(id_img, imagem):
+    foto = PATH_BKP+"//"+str(id_img)+".jpg" #salvar no windows
+    #foto = PATH_BKP+"/"+str(id_img)+".jpg" #salvar no ubuntu
+    
+    if not os.path.exists(PATH_BKP):
+        os.makedirs(PATH_BKP)
+        try:
+            imagem.save(foto)
+        except:
+            print("Erro ao salvar a imagem "+str(id_img)+".")
+    else:
+        if not os.path.exists(foto):
+            try:
+                imagem.save(foto)
+            except:
+                print("Erro ao salvar a imagem "+str(id_img)+".")
+            
+def salva_imagem():
+    
+    id_img  = 0
+    df_data = pd.read_csv(PATH)
+    df_data.set_index('id_imagem', inplace=True) # tornando a coluna id_imagem em indice
+    
+    for i in glob.glob(BASE_UFJF):
+
+        try:
+            img = Image.open(i)
+        except:
+            print("Imagem "+nome(i)+" não pode ser aberta.")
+            
+        id_img = int(nome(i)) # transforma id da imagem em inteiro antes de criar o diretorio
+        
+        try:
+            df_data.loc[id_img].values # se a imagem tá na planilha de rotulação, ela é salva na pasta
+            #img = img.resize((LARGURA, ALTURA), PIL.Image.ANTIALIAS)
+            img = cropFaces(i)
+            criarDiretorio(id_img, img) # armazena a imagem encontrada no csv
+        except:
+            continue
+
+def busca_csv(id_img, objImg):
+    
+    df_data = pd.read_csv(PATH)
+    df_data.set_index('id_imagem', inplace=True) # tornando a coluna id_imagem em indice
+        
+    try:
+        caracteristicas = df_data.loc[id_img].values
+        return caracteristicas
+    except:
+        print("Imagem"+str(id_img)+" não encontrada.\n")
+        
+def load_data(DIRETORIO):
+    
+    x       = [] # lista das imagens
+    y       = [] # lista de atributos das imagens (corCabelo, corOlhos...)
+    id_img  = 0
+    
+    for i in glob.glob(DIRETORIO):
+        
+        img             = Image.open(i)
+        pixels          = asarray(img).astype('float32') #transforma a imagem em um array de pixels
+        pixels          = pixels/pixels.max()
+        id_img          = int(nome(i)) # transforma id da imagem em inteiro antes de passar pra busca
+        caracteristicas = busca_csv(id_img, img)
+        x.append(pixels)
+        y.append(caracteristicas)
+        
+    return x,y
+
+print("Iniciando...")
+salva_imagem()
+#x_treino,y_treino = load_data(CONJ_TREINO)
+#x_teste,y_teste   = load_data(CONJ_TESTE)
+
+#data = (x_treino, y_treino), (x_teste,y_teste)
+
+#print(data)
+#print(y_treino[0])
+#pyplot.imshow(x_treino[0])
